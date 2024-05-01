6a25d45e20778e1017c48d59685cea547235605d
Omer <ok243@live.mdx.ac.uk>
2024-05-01 15:23:56 +0100

    edit app

--- App.tsx
+++ App.tsx
@@ -12,120 +12,79 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 const Stack = createStackNavigator(); //register/login
 const Tab = createBottomTabNavigator();
 const AuthContext = createContext({
-  hasUser: false,
-  setUser: () => {},
+    hasUser: false,
+    setUser: () => {},
 });
 
 // -------------------- BEFORE SIGN IN --------------------
 
 export default function SignInScreen() {
-  const { setUser } = useContext(AuthContext);
-  // const [email, setEmail] = useState('');1
-  // const [password, setPassword] = useState('');
-  return (
-      <View style={{gap: 20}}>
-        <TextInput label="Email" />
-        <TextInput label="Password" />
-        <Button  mode="elevated" onPress={() => setUser(true)}> Continue </Button>
-        <Divider style={{marginTop: "60%"}} />
-        <Button mode="outlined">      <Ionicons name="google" size={32} color="black" />
-          Continue with Google </Button>
-        <Button mode="outlined"> Continue with Facebook </Button>
-      </View>
-  );
+    const { setUser } = useContext(AuthContext);
+    // const [email, setEmail] = useState('');1
+    // const [password, setPassword] = useState('');
+    return (
+        <View style={{gap: 20}}>
+            <TextInput label="Email" />
+            <TextInput label="Password" />
+            <Button  mode="elevated" onPress={() => setUser(true)}> Continue </Button>
+            <Divider style={{marginTop: "60%"}} />
+            <Button mode="outlined">      <Ionicons name="google" size={32} color="black" />
+                Continue with Google </Button>
+            <Button mode="outlined"> Continue with Facebook </Button>
+        </View>
+    );
 }
 
 // -------------------- AFTER SIGN IN --------------------
 const Home = () => {
-  return (
-      <View>
-      </View>
-  )
+    return (
+        <View>
+        </View>
+    )
 }
 
 const Profile = () => {
-  const { setUser } = useContext(AuthContext);
-  return (
-      <View>
-        <Button mode="elevated" onPress={() => setUser(false)}> Log Out </Button>
-      </View>
-  );
+    const { setUser } = useContext(AuthContext);
+    return (
+        <View>
+            <Button mode="elevated" onPress={() => setUser(false)}> Log Out </Button>
+        </View>
+    );
 }
 
 const HomeTabs = () => {
-  return (
-      <Tab.Navigator screenOptions={{tabBarVisible:true, headerShown: false}} tabBar={({ navigation, state, descriptors, insets }) => (
-          <BottomNavigation.Bar
-              navigationState={state}
-              safeAreaInsets={insets}
-              onTabPress={({ route, preventDefault }) => {
-                const event = navigation.emit({
-                  type: 'tabPress',
-                  target: route.key,
-                  canPreventDefault: true,
-                });
-
-                if (event.defaultPrevented) {
-                  preventDefault();
-                } else {
-                  navigation.dispatch({
-                    ...CommonActions.navigate(route.name, route.params),
-                    target: state.key,
-                  });
-                }
-              }}
-              renderIcon={({ route, focused, color }) => {
-                const { options } = descriptors[route.key];
-                if (options.tabBarIcon) {
-                  return options.tabBarIcon({ focused, color, size: 24 });
-                }
-
-                return null;
-              }}
-              getLabelText={({ route }) => {
-                const { options } = descriptors[route.key];
-                const label =
-                    options.tabBarLabel !== undefined
-                        ? options.tabBarLabel
-                        : options.title !== undefined
-                            ? options.title
-                            : route.title;
-
-                return label;
-              }}
-          />
-      )}
-      >
-        <Tab.Screen name="Home" component={Home}/>
-        <Tab.Screen name="Profile" component={Profile}/>
-      </Tab.Navigator>
-  );
+    return (
+        <Tab.Navigator screenOptions={{tabBarVisible:true, headerShown: false}}>
+            <Tab.Screen name="Home" component={Home}/>
+            <Tab.Screen name="Profile" component={Profile}/>
+        </Tab.Navigator>
+    );
 }
 
 // ----------------- MAIN NAVIGATOR -------------------
 const MainNavigator = () => {
-  const { hasUser } = useContext(AuthContext);
+    const { hasUser } = useContext(AuthContext);
 
-  return (
-      <NavigationContainer>
-        <Stack.Navigator>
-          {hasUser ?
-              <Stack.Screen name="Home Tabs" component={HomeTabs} options={{headerShown: false,}}/>
-              :
-              <Stack.Screen name="Sign In" component={SignInScreen}/>
-          }
-        </Stack.Navigator>
-      </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                {hasUser ?
+                    <Stack.Screen name="Home Tabs" component={HomeTabs} options={{headerShown: false,}}/>
+                    :
+                    <Stack.Screen name="Sign In" component={SignInScreen}/>
+                }
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 };
 
 export default function App() {
-  const [hasUser, setUser] = useState(false);
-  return (
-      <AuthContext.Provider value={{hasUser, setUser}}>
-        <MainNavigator/>
-      </AuthContext.Provider>
-  );
+    const [hasUser, setUser] = useState(false);
+    return (
+        <AuthContext.Provider value={{hasUser, setUser}}>
+            <MainNavigator/>
+        </AuthContext.Provider>
+    );
 }
 
 // const styles = StyleSheet.create({
