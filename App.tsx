85b71b1a9812abaf6e7bbbff2f2d932123ee1252
Omer <ok243@live.mdx.ac.uk>
2024-05-02 09:00:16 +0100

    replace files

--- App.tsx
+++ App.tsx
@@ -1,104 +1,20 @@
-import React, { useState, createContext, useContext } from 'react';
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
-import { NavigationContainer, CommonActions } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-// You can import supported modules from npm
-import { TextInput, Card, BottomNavigation, Button, Divider } from 'react-native-paper';
-// or any files within the Snack
-import AssetExample from './components/AssetExample';
-import Ionicons from '@expo/vector-icons/Ionicons';
-
-const Stack = createStackNavigator(); //register/login
-const Tab = createBottomTabNavigator();
-const AuthContext = createContext({
-    hasUser: false,
-    setUser: () => {},
-});
-
-// -------------------- BEFORE SIGN IN --------------------
-
-export default function SignInScreen() {
-    const { setUser } = useContext(AuthContext);
-    // const [email, setEmail] = useState('');1
-    // const [password, setPassword] = useState('');
-    return (
-        <View style={{gap: 20}}>
-            <TextInput label="Email" />
-            <TextInput label="Password" />
-            <Button  mode="elevated" onPress={() => setUser(true)}> Continue </Button>
-            <Divider style={{marginTop: "60%"}} />
-            <Button mode="outlined">      <Ionicons name="google" size={32} color="black" />
-                Continue with Google </Button>
-            <Button mode="outlined"> Continue with Facebook </Button>
-        </View>
-    );
-}
-
-// -------------------- AFTER SIGN IN --------------------
-const Home = () => {
-    return (
-        <View>
-        </View>
-    )
-}
-
-const Profile = () => {
-    const { setUser } = useContext(AuthContext);
-    return (
-        <View>
-            <Button mode="elevated" onPress={() => setUser(false)}> Log Out </Button>
-        </View>
-    );
-}
-
-const HomeTabs = () => {
-    return (
-        <Tab.Navigator screenOptions={{tabBarVisible:true, headerShown: false}}>
-            <Tab.Screen name="Home" component={Home}/>
-            <Tab.Screen name="Profile" component={Profile}/>
-        </Tab.Navigator>
-    );
-}
-
-// ----------------- MAIN NAVIGATOR -------------------
-const MainNavigator = () => {
-    const { hasUser } = useContext(AuthContext);
-
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                {hasUser ?
-                    <Stack.Screen name="Home Tabs" component={HomeTabs} options={{headerShown: false,}}/>
-                    :
-                    <Stack.Screen name="Sign In" component={SignInScreen}/>
-                }
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
-};
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-    const [hasUser, setUser] = useState(false);
-    return (
-        <AuthContext.Provider value={{hasUser, setUser}}>
-            <MainNavigator/>
-        </AuthContext.Provider>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     backgroundColor: '#ecf0f1',
-//     padding: 8,
-//   },
-//   paragraph: {
-//     margin: 24,
-//     fontSize: 18,
-//     fontWeight: 'bold',
-//     textAlign: 'center',
-//   },
-// });
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
