4ff994d001146b685e32a282e0a7f92a0efd4f8c
Omer <ok243@live.mdx.ac.uk>
2024-05-11 15:32:31 +0100

    add more HomeScreen elements

--- components/Home.tsx
+++ components/Home.tsx
@@ -18,7 +18,8 @@ const styles: StyleSheet = {
         flex: 1,
     },
     topPart: {
-        flex: 1,
+        // flex: 1,
+        display: 'flex',
         backgroundColor: 'black',
         alignItems: 'center',
         justifyContent: 'center',
@@ -28,45 +29,63 @@ const styles: StyleSheet = {
         backgroundColor: 'white',
         alignItems: 'center',
     },
+    topTitle: {
+        fontSize: 48,
+        color: 'white',
+    },
     title: {
         fontSize: 48,
-    }
+    },
+    avatar: {
+        // width: 80,
+        height: 80,
+    },
+    card: {
+    },
 }
 
 export const HomeScreen = () => {
     return (
         <ScrollView style={styles.container}>
             <View style={styles.topPart}>
-                <Text style={styles.title}>BarbWebsite</Text>
+                <Text style={styles.topTitle}>BarbWebsite</Text>
                 <TextInput label={"Search"}/>
                 <ScrollView horizontal={true} showHorizontalScrollIndicator={false}>
-                    <TouchableRipple onPress={()=> console.log("1 Clicked")} rippleColor="rgba(0, 0, 0, .32)">
+                    <TouchableRipple style={styles.avatar} onPress={()=> console.log("1 Clicked")} rippleColor="rgba(0, 0, 0, .32)">
                         <Avatar.Text size={64} label="HAIR" />
                     </TouchableRipple>
-                    <TouchableRipple onPress={()=> console.log("2 Clicked")} rippleColor="rgba(0, 0, 0, .32)">
+                    <TouchableRipple style={styles.avatar} onPress={()=> console.log("2 Clicked")} rippleColor="rgba(0, 0, 0, .32)">
                         <Avatar.Text size={64} label="HAIR" />
                     </TouchableRipple>
-                    <TouchableRipple onPress={()=> console.log("Clicked")} rippleColor="rgba(0, 0, 0, .32)">
+                    <TouchableRipple style={styles.avatar} onPress={()=> console.log("Clicked")} rippleColor="rgba(0, 0, 0, .32)">
                         <Avatar.Text size={64} label="HAIR" />
                     </TouchableRipple>
-                    {/*<Button mode={"outlined"} onPress={() => console.log("Badge 1 Pressed")}>Badge 1</Button>*/}
-                    {/*<Button mode={"outlined"} onPress={() => console.log("Badge 2 Pressed")}>Badge 2</Button>*/}
-                    {/*<Button mode={"outlined"} onPress={() => console.log("Badge 3 Pressed")}>Badge 3</Button>*/}
-                    {/*<Button mode={"outlined"} onPress={() => console.log("Badge 1 Pressed")}>Badge 4</Button>*/}
-                    {/*<Button mode={"outlined"} onPress={() => console.log("Badge 2 Pressed")}>Badge 5</Button>*/}
-                    {/*<Button mode={"outlined"} onPress={() => console.log("Badge 3 Pressed")}>Badge 6</Button>*/}
                 </ScrollView>
-                <Button mode={"elevated"} onPress={() => console.log("Go to Appointments clicked")}>Go to Schedules</Button>
+                <Divider/>
+                <Text style={{color: "white"}}>No Appointments...</Text>
+                <Button mode={"elevated"} onPress={() => console.log("Go to Appointments clicked")}>Go to Appointments</Button>
             </View>
             <View style={styles.bottomPart}>
                 <Text style={styles.title}>New Barbers</Text>
                 <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-                    {/*    Cards here */}
-                </ScrollView>
-                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-                    {/*    Cards here */}
+                    <Card style={styles.card}>
+                        <Card.Title title={"BarberHire 1"}/>
+                        <Card.Content>
+                            <Text>Lorem Ipsum content.</Text>
+                        </Card.Content>
+                        <Card.Cover source={{uri: "https://heygoldie.com/blog/wp-content/uploads/2021/12/barber-shop-decor-ideas.jpg"}}/>
+                    </Card>
                 </ScrollView>
                 <Text style={styles.title}>Recommended</Text>
+                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                    <Card style={styles.card}>
+                        <Card.Title title={"Recommended 1"}/>
+                        <Card.Content>
+                            <Text>Lorem Ipsum content.</Text>
+                        </Card.Content>
+                        <Card.Cover source={{uri: "https://heygoldie.com/blog/wp-content/uploads/2021/12/barber-shop-decor-ideas.jpg"}}/>
+                    </Card>
+                </ScrollView>
             </View>
         </ScrollView>
     )
